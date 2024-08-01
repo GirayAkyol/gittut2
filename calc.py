8a4465cf5eba011be505b4c65d8d98b01f2b232f
gitu <gitu@lgq>
2024-08-01 03:41:01 +0300

    implement division

--- calc.py
+++ calc.py
@@ -19,6 +19,9 @@ def main():
     elif '*' in expression:
         num1, num2 = expression.split('*')
         result = float(num1) * float(num2)
+    elif '/' in expression:
+        num1, num2 = expression.split('/')
+        result = float(num1) / float(num2)
     else:
         print("Unsupported operator. Use +, -, *")
         return
